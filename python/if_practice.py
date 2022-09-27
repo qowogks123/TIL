2c036d3a471bdbff80bda8c48563fdde253a610c
qowogks123 <byh2589@naver.com>
2022-09-27 18:08:05 +0900

    conf: Rename the file
    Rename the python file 'if.py' to 'if_practice.py'

---
+++ python/if_practice.py
@@ -0,0 +1,25 @@
+# 143
+
+number = int(input('Enter the number : ' ))
+
+if number%2==0:
+    print('even number')
+else:
+    print('odd number')
+
+# 144
+
+number = int(input('Enter the number:'))
+
+if (number//10000) <= 54:
+    print('old')
+
+
+# 145
+
+num = 13
+
+if(num%2==0) | (num%3==0):
+    print('num은 2또는3의 배수')
+
+    
